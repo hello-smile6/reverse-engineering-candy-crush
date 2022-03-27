a407e1c249a95efdc1f74eea77663350b4d1191d
hello-smile6 <73048226+hello-smile6@users.noreply.github.com>
2022-03-26 18:40:49 -0700

    Use blank instead of emscriptenvars

--- candycrushsaga.js
+++ candycrushsaga.js
@@ -1807,7 +1807,7 @@ var ASM_CONSTS = {
     var apiName = UTF8ToString($0);
     var url = "";
     if (apiName === "server") {
-      url = emscriptenvars?.apiUrl || "";
+      url = /* emscriptenvars?.apiUrl  || */ "";
       // TODO: figure out the API URL
     } else if (apiName === "servicelayer") {
       url = Module["ksdk.slayerApiUrl"] || "";
